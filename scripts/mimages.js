a0a994633e375486fc67fc119e2a9710874c6800
tobiaspreecha <craftyhorizonstudios@gmail.com>
2017-11-01 20:50:22 +0100

    d
    d

--- scripts/mimages.js
+++ scripts/mimages.js
@@ -1,42 +1,54 @@
-$(document).ready(function(){
-    animateIMG();
-    
-});
- 
+(function(){
 
-function makeNewPosition(){
-    
-    // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height() - 50;
-    var w = $(window).width() - 50;
-    
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-    
-    return [nh,nw];    
-    
+function init(){
+     var canvas = document.getElementsByTagName('canvas')[0];
+     var c = canvas.getContext('2d');
+
+     var container = {x:100,y:100,width:1200,height:800};
+     var circles = [{x:400,y:400,r:50,color:25,vx:6,vy:10},
+                    {x:500,y:300,r:100,color:125,vx:2,vy:-8},
+                    {x:800,y:350,r:25,color:285,vx:20,vy:-20},
+                    {x:800,y:700,r:75,color:325,vx:13,vy:-8},
+                    {x:400,y:500,r:120,color:175,vx:-4,vy:-6}
+     ];
+
+
+     function draw(){
+         c.fillStyle = 'black';
+         c.strokeStyle = 'black';
+         c.fillRect(container.x,container.y,container.width,container.height);
+         //c.clearRect(container.x,container.y,container.width,container.height);
+         //c.strokeRect(container.x,container.y,container.width,container.height);
+
+         for(var i=0; i <circles.length; i++){
+             c.fillStyle = 'hsl(' + circles[i].color + ',100%,50%)';
+             c.beginPath();
+             c.arc(circles[i].x,circles[i].y,circles[i].r,0,2*Math.PI,false);
+             c.fill();
+
+             if((circles[i].x + circles[i].vx + circles[i].r > container.x + container.width) || (circles[i].x - circles[i].r + circles[i].vx < container.x)){
+                circles[i].vx = - circles[i].vx;
+             }
+             if((circles[i].y + circles[i].vy + circles[i].r > container.y + container.height) || (circles[i].y - circles[i].r + circles[i].vy < container.y)){
+                 circles[i].vy = - circles[i].vy;
+             }
+             circles[i].x +=circles[i].vx;
+             circles[i].y +=circles[i].vy;
+         }
+
+
+
+         requestAnimationFrame(draw);
+
+     }
+
+
+    requestAnimationFrame(draw);
+
+
 }
 
-function animateIMG(){
-    var newq = makeNewPosition();
-    var oldq = $('img').offset();
-    var speed = calcSpeed([oldq.top, oldq.left], newq);
-    
-    $('img').animate({ top: newq[0], left: newq[1] }, speed, function(){
-      animateIMG();        
-    });
-    
-};
+//invoke function init once document is fully loaded
+window.addEventListener('load',init,false);
 
-function calcSpeed(prev, next) {
-    
-    var x = Math.abs(prev[1] - next[1]);
-    var y = Math.abs(prev[0] - next[0]);
-    var greatest = x > y ? x : y;
-    
-    var speedModifier = 0.1;// control the speed here 
-
-    var speed = Math.ceil(greatest/speedModifier);
-    return speed;
-
-}
+}());  //self invoking function
